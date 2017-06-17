18471a49029a2f681bfc4983021de1b5aa36ff61
shumaosi <1315338189@qq.com>
2017-06-17 16:53:33 +0800

    添加开机启动页面 Signed-off-by: shumaosi <1315338189@qq.com>

--- app/src/main/java/com/szss/androidapp/guide/LauncherActivity.java
+++ app/src/main/java/com/szss/androidapp/guide/LauncherActivity.java
@@ -6,20 +6,24 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.szss.androidapp.R;
 import com.szss.androidapp.base.BaseActivity;
+import com.szss.androidapp.util.DensityUtil;
 
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by wuwei on 2017/6/17.
  */
 
-public class LauncherActivity extends BaseActivity {
+public class LauncherActivity extends BaseActivity implements ViewPager.OnPageChangeListener {
 
 	private ViewPager mViewPager;
-	private List<Fragment> mFragments;
+	private ArrayList<Fragment> mFragments;
 	private LauncherViewPagerAdapter mLauncherViewPagerAdapter;
 	private ImageView[] tips;
 	private int currentSelect;
@@ -27,6 +31,9 @@ public class LauncherActivity extends BaseActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.luancher_view);
+		initTips();
+		initViewPager();
 	}
 
 	@Override
@@ -35,24 +42,82 @@ public class LauncherActivity extends BaseActivity {
 	}
 
 	private void initTips() {
-		tips = new ImageView[3];
-//		for ()
+		ViewGroup viewGroup = (ViewGroup) findViewById(R.id.luancher_view_tips_viewgroup);
+		tips = new ImageView[4];
+		for (int i = 0; i < tips.length; i++) {
+			ImageView imageView = new ImageView(this);
+			imageView.setLayoutParams(new ViewGroup.LayoutParams(DensityUtil.px2dip(this,10), DensityUtil.px2dip(this,10)));
+			if (i == 0) {
+				imageView.setBackgroundResource(R.drawable.page_indicator_focused);
+			} else {
+				imageView.setBackgroundResource(R.drawable.page_indicator_unfocused);
+			}
+			tips[i] = imageView;
+			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+			layoutParams.leftMargin = DensityUtil.px2dip(this,30);
+			layoutParams.rightMargin = DensityUtil.px2dip(this,30);
+			viewGroup.addView(imageView,layoutParams);
+		}
 	}
+
+	private void initViewPager() {
+		mFragments = new ArrayList<>();
+		mFragments.add(LauncherFragment.newInstance(0));
+		mFragments.add(LauncherFragment.newInstance(1));
+		mFragments.add(LauncherFragment.newInstance(2));
+		mFragments.add(LauncherFragment.newInstance(3));
+		mViewPager = (ViewPager) findViewById(R.id.luancher_view_viewpager);
+		mLauncherViewPagerAdapter = new LauncherViewPagerAdapter(getSupportFragmentManager(), mFragments);
+		mViewPager.setAdapter(mLauncherViewPagerAdapter);
+		mViewPager.setOffscreenPageLimit(2);
+		mViewPager.setCurrentItem(0);
+		mViewPager.addOnPageChangeListener(this);
+	}
+
+	private void setTipsBackground(int selectPosition) {
+		for (int i = 0; i < tips.length; i++) {
+			if (i == selectPosition) {
+				tips[i].setBackgroundResource(R.drawable.page_indicator_focused);
+			} else {
+				tips[i].setBackgroundResource(R.drawable.page_indicator_unfocused);
+			}
+		}
+	}
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+		setTipsBackground(position);
+		currentSelect = position;
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int state) {
+
+	}
+
 
 	private class LauncherViewPagerAdapter extends FragmentStatePagerAdapter {
 
-		public LauncherViewPagerAdapter(FragmentManager fm) {
+		private ArrayList<Fragment> mFragments;
+
+		public LauncherViewPagerAdapter(FragmentManager fm, ArrayList<Fragment> fragments) {
 			super(fm);
+			this.mFragments = fragments;
 		}
 
 		@Override
 		public Fragment getItem(int position) {
-			return null;
+			return mFragments.get(position);
 		}
 
 		@Override
 		public int getCount() {
-			return 0;
+			return mFragments.size();
 		}
 	}
 
